f7e8e042ad6a28b230907facdd9709b45df791d4
Richard Arocutipa Lope <richardreyesarocutipalope@gmail.com>
2023-08-28 23:37:32 -0500

    Start teslo-shop

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
-import { PokemonModule } from './pokemon/pokemon.module';
-import { MongooseModule } from '@nestjs/mongoose';
-import { CommonModule } from './common/common.module';
-import { SeedModule } from './seed/seed.module';
-import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  controllers: [],
-  providers: [],
-  imports: [
-    ConfigModule.forRoot(),
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'public'),
-    }),
-
-    MongooseModule.forRoot(process.env.MONGODB),
-    PokemonModule,
-    CommonModule,
-    SeedModule,
-  ],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
